cced917270fd893a26891e8b788f7af23a7d3bc9
ShoulderToys <louisboss31@gmail.com>
2024-11-08 12:11:57 -0600

    CREACION DE PROYECTO BASE DE FLASK

---
+++ myapp/task/controllers.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+taskRoute = Blueprint('task', __name__, url_prefix='/task')
+#Creacion de rutas en el modulo rutas
+@taskRoute.route('/')
+def index():
+    return 'PRUEBITA 1'
+@taskRoute.route('/<int:id>') #devuelve un registro especifico de acuerdo al id(#)
+def show(id:int):
+    return 'Show ' +str(id)
+@taskRoute.route('/delete/<int:id>')
+def delete(id:int):
+    return 'delete ' +str(id)
+@taskRoute.route('/create', methods=('GET','POST'))
+def create():
+    return 'Create '
+@taskRoute.route('/update/<int:id>', methods=['GET','POST'])
+def update(id:int):
+    return 'update '+str(id)
